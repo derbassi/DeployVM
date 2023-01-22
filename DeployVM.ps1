99d44de9b543fdc96671cbe22aa24b3ef6a9ab69
badder <derbassi@gmail.com>
2023-01-22 20:49:11 +0100

    ComboBox Filled, Form's icon added

--- DeployVM.ps1
+++ DeployVM.ps1
@@ -15,7 +15,7 @@ Name="VMDeploy" Title="VM Deploy" Height="452" Width="464" WindowStartupLocation
 <Grid Background="#FFB5B188" Width="464" Margin="0,0,0,1">
         <Rectangle HorizontalAlignment="Center" Height="94" Stroke="Black" VerticalAlignment="Top" Width="452" Margin="0,4,0,0"/>
         <Label Content="Deploy VMware VM" HorizontalAlignment="Left" Margin="100,26,0,0" VerticalAlignment="Top" FontFamily="Britannic Bold" FontSize="36" Width="350" FontWeight="Bold"/>
-        <Image Height="77" Width="87" HorizontalAlignment="Left" VerticalAlignment="Top" Margin="12,12,0,0" Source="C:\Ps\DeployVM.png" Stretch="Fill"/>
+        <Image Height="77" Width="87" HorizontalAlignment="Left" VerticalAlignment="Top" Margin="12,12,0,0" Source="C:\gitHub\DeployVM\DeployVM.png" Stretch="Fill"/>
         <GroupBox Name="gbServer" Height="287" Margin="6,112,5,0" VerticalAlignment="Top" Header=" VM Caracteristics"/>
         <TextBlock Name="lblVMfolder" HorizontalAlignment="Left" Margin="26,165,0,0" TextWrapping="Wrap" Text="Folder >>" VerticalAlignment="Top" FontFamily="Consolas" Width="60" Height="16"/>
         <ComboBox Name="tbVMfolder" Width="344" Height="22" Margin="92,161,0,0" HorizontalAlignment="Left" Grid.Column="0" Grid.Row="0" VerticalAlignment="Top" FontFamily="Consolas" />
@@ -24,10 +24,11 @@ Name="VMDeploy" Title="VM Deploy" Height="452" Width="464" WindowStartupLocation
         <TextBlock Name="lblDisk1" HorizontalAlignment="Left" Margin="38,266,0,0" TextWrapping="Wrap" Text="Disk 1 >>" VerticalAlignment="Top" Width="60" Height="16" FontFamily="Consolas"/>
         <TextBlock Name="lblCore" HorizontalAlignment="Left" Margin="246,313,0,0" TextWrapping="Wrap" Text="# of Cores >>" VerticalAlignment="Top" Width="86" Height="16" FontFamily="Consolas"/>
         <TextBlock Name="lblDisk2" HorizontalAlignment="Left" Margin="244,266,0,0" TextWrapping="Wrap" Text="Disk 2 >>" VerticalAlignment="Top" Width="60" Height="16" FontFamily="Consolas"/>
-        <TextBlock Name="lblOS" Margin="26,196,0,0" TextWrapping="Wrap" Text="Operating System >>" VerticalAlignment="Top" FontFamily="Consolas" Width="126" Height="16" HorizontalAlignment="Left"/>
+        <TextBlock Name="lblOS" Margin="26,196,0,0" TextWrapping="Wrap" Text="Operation System >>" VerticalAlignment="Top" FontFamily="Consolas" Width="126" Height="16" HorizontalAlignment="Left"/>
         <ComboBox Name="tblOS" Width="282" Height="22" Margin="154,192,0,0" HorizontalAlignment="Left" VerticalAlignment="Top" Grid.Column="0" Grid.Row="0" FontFamily="Consolas" />
         <TextBlock Name="lblOSVersion" Margin="26,229,0,0" TextWrapping="Wrap" Text="Version / Distribution >>" VerticalAlignment="Top" FontFamily="Consolas" Width="166" Height="16" HorizontalAlignment="Left"/>
         <TextBox Name="tbVMName" HorizontalAlignment="Left" Margin="92,132,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="344" Height="22" FontFamily="Consolas"/>
+        <Rectangle HorizontalAlignment="Center" Height="41" Margin="0,254,0,0" Stroke="Black" VerticalAlignment="Top" Width="408"/>
         <TextBox Name="tbDisk1" HorizontalAlignment="Left" Margin="102,265,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="112" Height="18" FontFamily="Consolas"/>
         <TextBox Name="tbDisk2" HorizontalAlignment="Left" Margin="308,265,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="112" Height="18" FontFamily="Consolas"/>
         <TextBlock Name="lblCapacity1" HorizontalAlignment="Left" Margin="198,267,0,0" TextWrapping="Wrap" Text="GB" VerticalAlignment="Top" Width="16" Height="13" FontFamily="Consolas" FontSize="10"/>
@@ -56,7 +57,7 @@ $myForm.FindName("btnClose").add_click({
 
 #Fill the ComboBoxes
 #List of Folders
-Get-Folder | ? {$_.Type -like "VM" -and -not ($_.name -in ("vm", "vCLs")) } | ForEach-Object {
+Get-Folder | ForEach-Object {$_.Type -like "VM" -and -not ($_.name -in ("vm", "vCLs")) } | ForEach-Object {
     $tbVMfolder.items.Add($_.Name)
     $tbVMfolder.SelectedIndex = 1
     } | Out-Null
